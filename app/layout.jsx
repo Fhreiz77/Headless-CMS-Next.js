7157f6f26f7689cf31093cd0f150af5333299ef4
Fhreiz <iqbalzia2425@gmail.com>
2026-03-16 13:22:30 +0700

    Ini adalah first project

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="eng">
+      <head>
+        <title>Learn Next.Js Fundamental</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
